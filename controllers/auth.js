6de7f17463131c112fcc4ddccfc39987292201a9
mhassan654 <hassansaava@gmail.com>
2021-12-12 16:40:38 +0300

    added user update details functionality and user update password

--- controllers/auth.js
+++ controllers/auth.js
@@ -99,6 +99,42 @@ exports.getMe = asyncHandler(async (req, res, next) =>{
   });
 });
 
+//@desc     Update user details
+// @method   PUT al/api/v1/auth/updatedetails
+//@access    Private
+exports.updateDetails = asyncHandler(async (req, res, next) =>{
+  const fieldsToUpdate ={
+    name: req.body.name,
+    email: req.body.email
+  }
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
+
+//@desc     Update user password
+// @method   POST al/api/v1/auth/me
+//@access    Private
+exports.updatePassword = asyncHandler(async (req, res, next) =>{
+  const user = await User.findById(req.user.id).select('+password');
+
+  // check current password
+  if(!(await user.matchPassword(req.body.currentPassword))){
+    return next(new ErrorResponse('Password is incorrect'));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
+});
+
 //@desc     Forgot Password
 // @method   POST al/api/v1/auth/forgotpassword
 //@access    Public
